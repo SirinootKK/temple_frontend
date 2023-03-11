61977bb70940f37cf543fb90b10c4c8deab73b11
Sirinoot Ketkham(Fukfang) <98077546+SirinootKK@users.noreply.github.com>
2023-03-11 15:16:02 +0700

    checkout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { CSVLink, CSVDownload } from "react-csv";
+import { getTemples } from "./actions/temples";
+import Select, { components } from "react-select";
+
+const header = [{ lebel: "temple name", key: "name" }];
 
 function App() {
+  const [data, setData] = useState({
+    ROY: [],
+    RAN: [],
+    RAY: [],
+    YAL: [],
+  });
+
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    getTemples("ROY,RAN,RAY,YAL", setData);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="">
+       <input type="checkbox" id="roy" name="roy" value="ROY-ED"/>
+       <input type="checkbox" id="ran" name="ran" value="ROY-ED"/>
+       <input type="checkbox" id="ray" name="ray" value="ROY-ED"/>
+       <input type="checkbox" id="yal" name="yal" value="ROY-ED"/>
+      </div>
+      {/* {data?.ROY?.map(val => (
+        <div>{val}</div>
+      ))} */}
+      {/* <button onClick={e => exportToCSV(e, data?.ROY, "roy-ed.csv")}>Download Roy Ed</button> */}
+      <CSVLink filename="roy-ed.csv" data={data.ROY} target="_blank">
+        Download
+      </CSVLink>
     </div>
   );
 }
